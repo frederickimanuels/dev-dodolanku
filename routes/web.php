108be2af70952a8857308ee4f1a3f9594f8f6e81
JuanFW <juan.bounche@gmail.com>
2022-05-24 17:12:37 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,14 @@ Auth::routes();
 // Middleware Auth
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/home2','FrontendController@index')->name('homepage');
+Route::get('/home2','FrontendController@index')->name('homepage2');
+
+Route::get('/login','FrontendController@login')->name('login');
+Route::get('/register','FrontendController@register')->name('register');
+
+
 route::get('/dashboard','FrontendController@dashboard')->name('dashboard');
+Route::get('/aboutus','FrontendController@aboutus')->name('aboutus');
 Route::get('/profile','FrontendController@profile')->name('profile');
+Route::get('/cart','FrontendController@cart')->name('cart');
+Route::get('/category','FrontendController@category')->name('category');
