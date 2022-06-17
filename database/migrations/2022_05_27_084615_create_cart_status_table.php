4596b20216ece0eda382d17699c0e24608d9a768
Frederick Imanuel Setiawan <frederick.bounche@gmail.com>
2022-06-17 18:17:52 +0700

    Migration fix

--- database/migrations/2022_05_27_084615_create_cart_status_table.php
+++ database/migrations/2022_05_27_084615_create_cart_status_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStatusCartsTable extends Migration
+class CreateCartStatusTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateStatusCartsTable extends Migration
      */
     public function up()
     {
-        Schema::create('status_carts', function (Blueprint $table) {
+        Schema::create('cart_status', function (Blueprint $table) {
             $table->unsignedBigInteger('status_id');
             $table->unsignedBigInteger('cart_id');
             $table->unique(['status_id','cart_id']);
@@ -30,6 +30,6 @@ class CreateStatusCartsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('status_carts');
+        Schema::dropIfExists('cart_status');
     }
 }
