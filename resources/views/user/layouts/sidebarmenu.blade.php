1fc4459ba92805dfaa3037a41074102b6240ff2f
JuanFW <juan.bounche@gmail.com>
2022-06-17 17:11:53 +0700

    push sidebar

---
+++ resources/views/user/layouts/sidebarmenu.blade.php
@@ -0,0 +1,17 @@
+<div class="col-lg-3 col-12 left-profile">
+    <div class="img-container">
+        <div class="img-inner">
+            <img src="{{asset('images/homepage/profile1.jpg')}}" alt="Avatar" class="img-profile-big">
+        </div>
+    </div>
+        <h2 class="profile-name" >{{ Auth::user()->name }}</h2>
+        <ul class="profile-menu-list">
+            {{-- <li>Wallet</li> --}}
+            <li>
+                <a href="{{ route('user.profile') }}">Profile</a>
+            </li>
+            <li>Orders</li>
+            <li>Address</li>
+            {{-- <li>Payment Methods</li> --}}
+        </ul>
+</div>
